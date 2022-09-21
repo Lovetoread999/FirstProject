8d8f6dba900105f5e37dc4b5e7bf2f1d5482c76b
Jeremiah Gregg <lovetoread99999@gmail.com>
2022-09-21 12:33:00 -0600

    first commit

---
+++ Notalesson.html.h
@@ -0,0 +1,16 @@
+<html>
+    
+<body>
+    
+    <ul>
+        
+        <li><a href="file:///C:/Users/user/OneDrive/Pictures/Saved%20Pictures/giphy.jpg.gif">Hello giphy</a>
+        </li> 
+	<li><a href="file:///C:/Users/user/OneDrive/Pictures/Screenshots/Screenshot%20(20).png">A file</a></li>
+        <a href="file:///C:/Users/user/OneDrive/Documents/Html/SomethingImade.html"><img src="file:///C:/Users/user/OneDrive/Pictures/Screenshots/Screenshot%20(20).png"width=600 height=400 />
+
+    </ul>
+    
+</body>
+    
+</html>
---
+++ lesson13.html
@@ -0,0 +1,9 @@
+<html>
+   <video width="320" height="240" Controls>
+    <source src="file:///C:/Users/user/OneDrive/Pictures/Camera%20Roll/WIN_20220710_18_08_30_Pro.mp4" type="video/mp4">
+    
+    Your browser does not support this video.
+        
+    </video>
+    
+</html>
